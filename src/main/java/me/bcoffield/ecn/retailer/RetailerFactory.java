f50a27540015bb81eabbc69184f5295771c14c96
Brien Coffield <coffbr01@gmail.com>
2020-09-22 23:50:54 -0500

    Go back to java 11 because FU Oracle

--- src/main/java/me/bcoffield/ecn/retailer/RetailerFactory.java
+++ src/main/java/me/bcoffield/ecn/retailer/RetailerFactory.java
@@ -7,13 +7,18 @@ public class RetailerFactory {
   public static IRetailer getRetailer(String urlStr) {
     try {
       URL url = new URL(urlStr);
-      return switch (url.getHost()) {
-        case "www.bestbuy.com" -> new BestBuy();
-        case "www.microcenter.com" -> new MicroCenter();
-        case "www.newegg.com" -> new Newegg();
-        case "www.nvidia.com" -> new Nvidia();
-        default -> throw new RuntimeException("Unknown site " + url.getHost());
-      };
+      switch (url.getHost()) {
+        case "www.bestbuy.com":
+          return new BestBuy();
+        case "www.microcenter.com":
+          return new MicroCenter();
+        case "www.newegg.com":
+          return new Newegg();
+        case "www.nvidia.com":
+          return new Nvidia();
+        default:
+          throw new RuntimeException("Unknown site " + url.getHost());
+      }
     } catch (MalformedURLException e) {
       e.printStackTrace();
       throw new RuntimeException("Malformed URL " + urlStr);
