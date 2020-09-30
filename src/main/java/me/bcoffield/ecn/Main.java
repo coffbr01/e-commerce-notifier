3ff4b2be2ba226c73a718ea9d7c05fa8b8b57ee8
Brien Coffield <coffbr01@gmail.com>
2020-09-29 19:04:42 -0500

    Add hours to delay output

--- src/main/java/me/bcoffield/ecn/Main.java
+++ src/main/java/me/bcoffield/ecn/Main.java
@@ -135,8 +135,9 @@ public class Main {
   private String toHumanReadableTime(long durationInMillis) {
     long second = (durationInMillis / 1000) % 60;
     long minute = (durationInMillis / (1000 * 60)) % 60;
+    long hour = (durationInMillis / (1000 * 60 * 60)) % 60;
 
-    return String.format("%02d:%02d", minute, second);
+    return String.format("%02d:%02d:%02d", hour, minute, second);
   }
 
   private INotifier getNotifier() {
