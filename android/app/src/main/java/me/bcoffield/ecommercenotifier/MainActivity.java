e75e5412348d259e1972fb7925d34b02da1483de
Brien Coffield <coffbr01@gmail.com>
2020-12-12 15:43:24 -0600

    Android notification from firebase data message

--- android/app/src/main/java/me/bcoffield/ecommercenotifier/MainActivity.java
+++ android/app/src/main/java/me/bcoffield/ecommercenotifier/MainActivity.java
@@ -1,5 +1,7 @@
 package me.bcoffield.ecommercenotifier;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,6 +22,8 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
 import me.bcoffield.ecommercenotifier.util.FirebaseUtils;
+
+import static me.bcoffield.ecommercenotifier.util.Constants.CHANNEL_ID;
 
 public class MainActivity extends AppCompatActivity {
     private static final int SIGN_IN_CODE = 37;
@@ -42,6 +46,20 @@ public class MainActivity extends AppCompatActivity {
             });
         }
         toggleSignIn(currentUser == null);
+        createNotificationChannel();
+    }
+
+    private void createNotificationChannel() {
+        CharSequence name = getString(R.string.channel_name);
+        String description = getString(R.string.channel_description);
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+        channel.setDescription(description);
+        // Register the channel with the system; you can't change the importance
+        // or other notification behaviors after this
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+
     }
 
     @Override
--- android/app/src/main/java/me/bcoffield/ecommercenotifier/service/MyFirebaseMessagingService.java
+++ android/app/src/main/java/me/bcoffield/ecommercenotifier/service/MyFirebaseMessagingService.java
@@ -1,15 +1,47 @@
 package me.bcoffield.ecommercenotifier.service;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.net.Uri;
+
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
+import me.bcoffield.ecommercenotifier.R;
 import me.bcoffield.ecommercenotifier.util.FirebaseUtils;
 
+import static me.bcoffield.ecommercenotifier.util.Constants.CHANNEL_ID;
+
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
     @Override
     public void onNewToken(@NonNull String token) {
         super.onNewToken(token);
         FirebaseUtils.getInstance().uploadToken(token);
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        String url = remoteMessage.getData().get("note");
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW);
+        browserIntent.setData(Uri.parse(url));
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1, browserIntent, PendingIntent.FLAG_ONE_SHOT);
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("In Stock!")
+                .setContentText(url)
+                .setPriority(NotificationCompat.PRIORITY_MAX)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Service.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notification);
     }
 }
